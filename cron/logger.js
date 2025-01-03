619428bb5b47ca46faeecdb35e16f922e57b2535
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2025-01-03 20:30:41 +0700

    fix(cron): correct log file path
    - Changed log file path from ./log/cron.log to /app/logs/scheduler.log

--- cron/logger.js
+++ cron/logger.js
@@ -4,7 +4,7 @@ const path = require("path");
 const logger = (message, additionalInfo) => {
   const now = new Date().toISOString();
   const logMessage = `${now} : ${message}\n ${additionalInfo} \n`;
-  fs.appendFileSync(path.join("./log/cron.log"), logMessage);
+  fs.appendFileSync(path.join("/app/logs/scheduler.log"), logMessage);
 };
 
 module.exports = logger;
