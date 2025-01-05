efc31e6b62342ae200fe5d5a503ded946db0e03f
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2025-01-05 20:30:30 +0700

    refactor(cron): simplify cron job implementation
    - Remove unnecessary cron library
    - Use async/await for cleaner code
    - Improve logging

--- cron/cron.js
+++ cron/cron.js
@@ -1,15 +1,8 @@
-var cron = require('node-cron');
 const mistral = require("./mistral");
 
-const cronExpression = process.env.CRON_EXPRESSION || '* * * * *';
 
-cron.schedule(cronExpression, () => {
-  console.log("Cron job is running", "");
+const main = async () => {
+  console.info(`${new Date().toISOString()} : Starting cron job`);
+};
 
-  const response = mistral();
-  response.then((res) => {
-    console.log("Cron job completed", res);
-  }).catch((err) => {
-    console.log("Cron job failed", err);
-  });
-});
+main();
